a1278d6e0ef5798e3b363d64145cd5a2f7fa13db
Mark Protsyuk <marcprotsyuk@gmail.com>
2023-07-25 14:46:03 -0700

    added linked list implementation for sorting algo

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+const numberSorter = new Sorter(numbersCollection);
+numberSorter.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("XtYbnMm");
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+const characterSorter = new Sorter(charactersCollection);
+characterSorter.sort();
 console.log(charactersCollection.data);
